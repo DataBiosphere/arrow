11980e80719bec216c1be3cdc92da8a0cc4cfcb6
Brian Reilly <breilly@broadinstitute.org>
2019-08-26 13:39:43 -0400

    default some options (for now) fix s3 signed URLs by _not_ quoting the parts

--- arrow/server.py
+++ arrow/server.py
@@ -2,7 +2,6 @@ import fastavro
 import urllib.request
 from sanic import response, Sanic
 from arrow.translate import translate
-from arrow.utils import sanitize_url
 
 app = Sanic('arrow')
 
@@ -14,10 +13,10 @@ async def status(request):
 
 @app.route('/avroToRawls', methods={'POST'})
 async def avro_to_rawls(request):
-    url = sanitize_url(request.json['url'])
+    url = request.json['url']
     avro = urllib.request.urlopen(url)
     reader = fastavro.reader(avro)
-    result = await translate(reader)
+    result = await translate(reader, {'b64-decode-enums': True, 'prefix-object-ids': True})
     return response.json(result)
 
 
