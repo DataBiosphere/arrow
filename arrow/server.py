a13d7b78fb793d160b78a778e522599fb9ebe974
Brian Reilly <breilly@broadinstitute.org>
2019-08-28 10:03:32 -0400

    add plumbing for options add warmup increase gunicorn timeout

--- arrow/server.py
+++ arrow/server.py
@@ -6,6 +6,11 @@ from arrow.translate import translate
 app = Sanic('arrow')
 
 
+@app.get('/_ah/warmup')
+async def warmup(request):
+    return response.text('OK')
+
+
 @app.get('/status')
 async def status(request):
     return response.text('OK')
@@ -14,9 +19,13 @@ async def status(request):
 @app.route('/avroToRawls', methods={'POST'})
 async def avro_to_rawls(request):
     url = request.json['url']
+    defaults = {'b64-decode-enums': True, 'prefix-object-ids': True}
+    request_options = request.json.get('options', {})
+    options = {**defaults, **request_options}
+
     avro = urllib.request.urlopen(url)
     reader = fastavro.reader(avro)
-    result = await translate(reader, {'b64-decode-enums': True, 'prefix-object-ids': True})
+    result = await translate(reader, options)
     return response.json(result)
 
 
