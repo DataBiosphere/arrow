64e5cef1c2232eb482729b2659a7c0c22a444e16
Brian Reilly <breilly@broadinstitute.org>
2019-09-11 09:34:13 -0400

    Improve error reporting [SATURN-1056] (#2)

--- arrow/server.py
+++ arrow/server.py
@@ -2,23 +2,31 @@ import fastavro
 import urllib.request
 from sanic import response, Sanic
 from arrow.translate import translate
+from arrow.util import catch_and_report_exceptions
 
 app = Sanic('arrow')
 
 
 @app.get('/_ah/warmup')
+@catch_and_report_exceptions()
 async def warmup(request):
     return response.text('OK')
 
 
 @app.get('/status')
+@catch_and_report_exceptions()
 async def status(request):
     return response.text('OK')
 
 
 @app.route('/avroToRawls', methods={'POST'})
+@catch_and_report_exceptions()
 async def avro_to_rawls(request):
-    url = request.json['url']
+    try:
+        url = request.json['url']
+    except KeyError:
+        return response.text("Missing required 'url' property", 400)
+
     defaults = {'b64-decode-enums': True, 'prefix-object-ids': True}
     request_options = request.json.get('options', {})
     options = {**defaults, **request_options}
