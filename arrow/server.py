bb91b8cae631d1b537b5bb5f6b120d11e8bbe25f
Brian Reilly <breilly@broadinstitute.org>
2019-09-12 09:47:54 -0400

    remove sanic-compress for simplicity because app engine already does this

--- arrow/server.py
+++ arrow/server.py
@@ -1,11 +1,9 @@
 import fastavro
 import urllib.request
 from sanic import response, Sanic
-from sanic_compress import Compress
 from arrow.translate import translate
 
 app = Sanic('arrow')
-Compress(app)
 
 
 @app.get('/_ah/warmup')
